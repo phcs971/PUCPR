4c551ebfafa3db9183f8efadc74ef689fe2c01d0
phcs971 <phcs.971@gmail.com>
2022-03-21 22:42:19 -0300

    IOSC - Modelo

---
+++ 7P/IOSC/Alunos AP1/Run_Simulink.m
@@ -0,0 +1,56 @@
+%% Step test
+% This script implements a test in simulink. It requires the 
+% following scripts:
+%
+% PM_dX_----- : ODE system.
+% -----_Block : Script to create a simulink block using PM_dX_-----.
+% -----_OpenLoop_Simulink : Simulink file to simulate a given model.
+%
+%
+%% Author
+% Gilberto Reynoso Meza (ORCid 0000-0002-8392-6225)
+% @gilreyme
+% http://www.researchgate.net/profile/Gilberto_Reynoso-Meza
+% Version: Aug/2019
+%
+%% Plot features
+
+close all;
+clear all;
+% Change default axes fonts.
+set(0,'DefaultAxesFontName', 'Arial')
+set(0,'DefaultAxesFontSize', 16)
+% Change default text fonts.
+set(0,'DefaultAxesFontName', 'Arial')
+set(0, 'DefaultAxesFontWeight','Bold')
+set(0,'DefaultTextFontSize', 16)
+
+
+%% Running OPEN LOOP simulation
+
+V_Profile=[00 0.0;
+          100 0.0;
+          100 0.4;
+          200 0.4;
+          ];
+     
+Time_sim=200;
+Ts=0.01;
+
+sim('SphericalTank');
+figure(1);
+
+%% Plotting time response
+
+subplot(211);
+plot(H_Read(:,1),H_Read(:,2),'b','linewidth',4);
+ylabel('Fluid Level [m]');
+ylim([-0.1 2.1]);
+grid on;
+
+subplot(212);
+plot(V(:,1),V(:,2),'color',[0, 0.5, 0],'linewidth',4);
+xlabel('Time [s]');
+ylabel('Valve Position [-]');
+ylim([-0.1 1.1]);
+grid on;
