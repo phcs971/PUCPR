8a22ebf488cc4c2dc25070a5c5c704bf532774c2
phcs971 <phcs.971@gmail.com>
2022-10-07 20:11:44 -0300

    Atv03 e Atv04 IAAM

--- 8P/IAAM/Atv03/main.py
+++ 8P/IAAM/Atv03/main.py
@@ -1,60 +1,27 @@
-from calendar import month_name
-from json import load
-from modulefinder import IMPORT_NAME
 from  scipy.io import loadmat
 import pandas as pd
-from sklearn import tree
-import cross_val_score
-
-
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
 
 def main():
-    inputs = pd.DataFrame(loadmat('Inputs-1.mat')['Inputs'])
+    inputs = pd.DataFrame(loadmat('Inputs-1.mat')['Inputs']).fillna(method="bfill")
     targets = pd.DataFrame(loadmat('Targets-1.mat')['Targets'])
-    print(inputs)
-    print(targets)
+
+    inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs, targets, test_size=0.5)
     
-    #decision tree wiht the inputs and targets
-    model = tree.DecisionTreeClassifier()
-    model = model.fit(inputs, targets)
-    print(model)
+    # MLPClassifier
+    mlp = MLPClassifier(hidden_layer_sizes=(50, 50, 50), max_iter=1000, verbose=True)
+    mlp.fit(inputs_train, targets_train.values.ravel())
+    predictions = mlp.predict(inputs_test)
+    print("MLPClassifier: ", mlp.score(inputs_test, targets_test))
 
-    #predict the inputs
-    print(model.predict(inputs))
-    print(targets)
+    print(confusion_matrix(targets_test, predictions))
+    print(classification_report(targets_test, predictions))
 
-    #cross validation
-    scores = cross_val_score(model, inputs, targets, cv=5)
-    print(scores)
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-    #decision tree wiht the inputs and targets
-    model = tree.DecisionTreeClassifier()
-    model = model.fit(inputs, targets)
-    print(model)
 
-    #predict the inputs
-    print(model.predict(inputs))
-    print(targets)
-
-    #cross validation
-    scores = cross_val_score(model, inputs, targets, cv=5)
-    print(scores)
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-    #decision tree wiht the inputs and targets
-    model = tree.DecisionTreeClassifier()
-    model = model.fit(inputs, targets)
-    print(model)
-
-    #predict the inputs
-    print(model.predict(inputs))
-    print(targets)
-
-    #cross validation
-    scores = cross_val_score(model, inputs, targets, cv=5)
-    print(scores)
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
 if __name__ == '__main__':
--- 8P/IAAM/atv04/main.py
+++ 8P/IAAM/atv04/main.py
@@ -1,3 +1,13 @@
 import pandas as pd
-data = pd.read_csv('data.csv')
-print(data)
+from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split
+
+def main():
+    data = pd.read_csv('data.csv')
+    inputs = data[['ts', 'vc']]
+    targets = data[['hot', 'rad']]
+    inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs,  targets, test_size=0.4)
+
+
+if __name__ == '__main__':
+    main()
